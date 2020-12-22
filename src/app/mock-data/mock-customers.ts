34bb0d09bf2e1ba6ea98146450fd7c0cbd43d05b
sebamawa <sebamawa@hotmail.com>
2020-12-21 21:27:46 -0300

    read json from filesystem

--- src/app/mock-data/mock-customers.ts
+++ src/app/mock-data/mock-customers.ts
@@ -1,13 +1,15 @@
 import { Customer } from '../interfaces/customer';
+import customers_json from './customers.json';
 
 // let CUSTOMERS = JSON.parse(localStorage.getItem("customers"));
 // if (!CUSTOMERS)
 //     CUSTOMERS = [];
 
-export const CUSTOMERS: Customer[] = [
-    { id: 1, name: 'Pablo' },
-    { id: 2, name: 'Mirna' },
-    { id: 3, name: 'Alejandro' }
-];
+export const CUSTOMERS: Customer[] = customers_json;
+// [
+//     { id: 1, name: 'Pablo' },
+//     { id: 2, name: 'Mirna' },
+//     { id: 3, name: 'Alejandro' }
+// ];
 
 // export { CUSTOMERS };
